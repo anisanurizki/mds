39c8910b53110f91027dd8728593a299f65a44e1
Anisa Nurizki <102646104+anisanurizki@users.noreply.github.com>
2022-06-12 07:00:32 +0700

    Update publish.R

--- publish.R
+++ publish.R
@@ -7,26 +7,27 @@ library(mongolite)
 ##Scraping Data
 url <- "https://harga-emas.org/1-gram/"
 emas <- read_html(url)
+data <- url %>% read_html() %>% html_table
+data <- data[[1]]
+View(data)
 
-#Financial Market (Daily)
-market = data[42:44,]
-colnames(market) <- market[1,]
-market = market[-1,]
+emas <- data[3:5,-c(3:4)]
 
 ##Menyimpan update data ke MongoDB Database
 #Menyiapkan koneksi
 connection_string = Sys.getenv("MONGODB_CONNECTION")
 
 #Markets
-pasar = mongo(collection="Pasar_Keuangan",
-              db="Indikator_Ekonomi",
+harga = mongo(collection="cluster0",
+              db="UAS-MDS",
               url=connection_string)
 pasar$insert(market)
+
 
 # Publish to Twitter
 ##Create Twitter token
 indikator_token <- create_token(
-  app = "Indikator Ekonomi",
+  app = "Harga Emas",
   consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
   access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
@@ -34,21 +35,17 @@ indikator_token <- create_token(
 )
 
 ##Tweet
-market_tweet <- paste0("Indikator Pasar Keuangan Indonesia",
-                       "\n",
-                       "\n",
-                       market[1,1], " periode ", market[1,2], " adalah ", market[1,3],
-                       " atau berubah sebesar ", 
-                       round(((as.numeric(market[1,3])-as.numeric(market[1,4]))/as.numeric(market[1,4])*100),2),
-                       " persen dari periode sebelumnya.",
-                       "\n",
-                       "\n",
-                       market[2,1], " periode ", market[2,2], " adalah ", market[2,3],
-                       " atau berubah sebesar ", 
-                       round(((as.numeric(sub(",", ".", market[2,3], fixed = TRUE))
-                               -as.numeric(sub(",", ".", market[2,4], fixed = TRUE)))
-                              /as.numeric(sub(",", ".", market[2,4], fixed = TRUE))*100),2),
-                       " persen dari periode sebelumnya.")
+emas_tweet <- paste0("Update" data[2,1],
+                     "\n",
+                     "\n",
+                     "USD:" data[3,2],
+                     "\n",
+                     "IDR:" data[5,2],
+                     "\n"
+                     "KURS:" data[4,2]
+                     "\n",
+                     "\n",
+                    "Update terakhir pada" data[6,2])
 
 ## Post the image to Twitter
-post_tweet(status = market_tweet, token = indikator_token)
+post_tweet(status = emas_tweet, token = indikator_token)
