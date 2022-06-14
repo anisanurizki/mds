175f7b1a94625695b6b14c41c57409de6c2660e6
Anisa Nurizki <102646104+anisanurizki@users.noreply.github.com>
2022-06-14 11:11:28 +0700

    Update gold.R

--- gold.R
+++ gold.R
@@ -5,31 +5,44 @@ library(rtweet)
 library(mongolite)
 
 ##Scraping Data
-url <- "https://harga-emas.org/1-gram/"
-emas <- read_html(url)
+url  <- "https://harga-emas.org/1-gram/"
+url2 <- "https://harga-emas.org/perak/"
+
+# gold
 data <- url %>% read_html() %>% html_table
 data <- data[[1]]
-View(data)
+gold <- data[3:6,-c(3:4)]
 
-emas <- data[3:6,-c(3:4)]
+#silver
+data2  <- url2 %>% read_html() %>% html_table
+data2  <- data2[[1]]
+silver <- data2[3:8,-c(4:5)]
 
 ##Menyimpan update data ke MongoDB Database
 #Menyiapkan koneksi
 connection_string = Sys.getenv("MONGODB_CONNECTION")
 
-#Markets
+#harga gold
 harga = mongo(
   collection = "Emas_24_Karat",
   db = "Harga_Emas",
   verbose = FALSE,
   options = ssl_options()
 )
-harga$insert(emas)
+harga$insert(gold)
 
+#harga silver
+harga2 = mongo(
+  collection = "Silver",
+  db = "Harga",
+  verbose = FALSE,
+  options = ssl_options()
+)
+harga2$insert(silver)
 
 # Publish to Twitter
 ##Create Twitter token
-indikator_token1 <- create_token(
+indikator_token <- create_token(
   app = "anrizki",
   consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
@@ -37,15 +50,36 @@ indikator_token1 <- create_token(
   access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 )
 
-##Tweet
-emas_tweet <- paste0("Update Harga 1 Gram Emas 24 Karat",
+##Tweet Gold
+gold_tweet <- paste0("Update Harga 1 Gram Emas 24 Karat",
                      "\n",
-                     data[6,2],
+                     gold[4,2],
                      "\n",
                      "\n",
-                     "USD: ", data[3,2],
+                     "USD: ", gold[1,2],
                      "\n",
-                     "IDR: ", data[5,2])
+                     "IDR: ", gold[3,2])
+
+##Tweet Silver
+silver_tweet <- paste0("Update Harga Perak",
+                       "\n",
+                       silver[6,1],
+                       "\n",
+                       "\n",
+                       "Per 1 Gram",
+                       "\n",
+                       "USD: ", silver[2,3],
+                       "\n",
+                       "IDR: ", silver[4,3],
+                      "\n",
+                       "\n",
+                       "Per 1 Ons",
+                       "\n",
+                       "USD: ", silver[1,3],
+                       "\n",
+                       "IDR: ", silver[3,3],
+                      )
 
 ## Post the image to Twitter
-post_tweet(status = emas_tweet, token = indikator_token1)
+post_tweet(status = gold_tweet, token = indikator_token)
+post_tweet(status = silver_tweet, token = indikator_token)
